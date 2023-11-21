049e21a1ab293185a0f92afb66bde6d07b77b1c1
Caio César <105644099+CaioCesar-04@users.noreply.github.com>
2023-11-21 10:49:32 -0300

    Update atividade5.m

--- atividade5.m
+++ atividade5.m
@@ -1,17 +1,26 @@
 function atividade5()
-  clc;
+clc;
 xval=14;
-m=2;
-n=6;
-x=[6,7,11,15,17,21,23,29,29,37,39];
-y=[26,21,29,14,21,15,7,7,13,0,3];
+m=9;
+n=10;
+x=[6,7,11,15,17,21,23,29,37,39];
+y=[26,21,29,14,21,15,7,13,0,3];
 if n>=m+1
-   [a]=quadrados(x,y,m,n)
+   [a]=quadrados(x,y,m,n);
    A=a(:,1:(end-1));
    b=a(:,end);
-   coef=A\b;
+   coef=inv(A)*b;
    f=polinomio(xval,coef);
-   [r,r2]=coeficientesR(x,y,coef)
+   [r,r2]=coeficientesR(x,y,coef);
+   plotGrafico(x,y,f,xval,coef)
+   fprintf("A ordem:%i\n",m);
+   fprintf("O valor do polinomio em x=14:%.6f\n",f);
+   fprintf("O valor de r2:%f\n",r2);
+   fprintf("O valor de r:%f\n",r);
+   fprintf("O valor do polinomio:");
+   for i=1:m+1
+     fprintf("%.6f*x^%i+",coef(i),i-1);
+   endfor
 endif
 
 endfunction
@@ -50,14 +59,23 @@ endfunction
 function y=polinomio(xval,coef)
   y=0;
   for i=1:numel(coef)
-    y=y+coef(i)*xval^(i-1);
+    y=y+coef(i)*xval^(i-1)
   endfor
 endfunction
   
-function plotGrafico(x,y,coef)
+function plotGrafico(x,y,f,xval,coef)
   aux1=x(1):0.1:x(end);
   aux2=zeros(size(aux1));
-  for
-    aux2(cont)=polinomio
-    endfor
-  endfunction
+  for cont=1:numel(aux1)
+    aux2(cont)=polinomio(aux1(cont),coef);
+  endfor
+  figure(1);
+  plot(aux1,aux2,'linewidth',2,'color','r');
+  hold on 
+  plot(x,y,'o','markerfacecolor','k','color','k');
+  hold on
+  plot(xval,f,'*','markerfacecolor','k','color','b')
+  hold off
+  set(gca,'fontsize',12);
+  
+endfunction
